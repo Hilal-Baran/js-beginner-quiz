8f57b5c321b80b313527fcf351d2f28bda568c53
hilalbaran <hilalinizci@hotmail.com>
2024-10-07 04:48:49 +0300

    ilk soru yanlışlar düzeltildi.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,10 +1,12 @@
-let score = 0;
 let totalQuestions = 10;
+let dogruSayisi=0;
+let yanlisSayisi=0;
 
-let answer1 = prompt("CSS açılımı nedir? \n a) Computer Style Sheets b) Cascading Style Sheets c) Creative Style Sheets d) Colorful Style Sheets");
-if (answer1 == b) {
-  score++;
-  alert("✔") 
-} else if (answer1 !== b) {
-  alert("✖")
+let answer1 = prompt('CSS açılımı nedir? \n a) Computer Style Sheets \n b) Cascading Style Sheets \n c) Creative Style Sheets d) Colorful Style Sheets');
+if (answer1 == 'b') {
+  dogruSayisi++;
+  alert("✔");
+} else if (answer1 !== 'b') {
+  alert("✖");
+  yanlisSayisi++;
 }
