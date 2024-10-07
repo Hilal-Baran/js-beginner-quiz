a87b8909a967247eae7e83016d3a1543b37f18e2
hilalbaran <hilalinizci@hotmail.com>
2024-10-07 04:41:55 +0300

    ilk soru yazıldı.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,10 @@
 let score = 0;
 let totalQuestions = 10;
 
+let answer1 = prompt("CSS açılımı nedir? \n a) Computer Style Sheets b) Cascading Style Sheets c) Creative Style Sheets d) Colorful Style Sheets");
+if (answer1 == b) {
+  score++;
+  alert("✔") 
+} else if (answer1 !== b) {
+  alert("✖")
+}
