7e339e2a211caeebce455a0176dafde82eee6672
hilalbaran <hilalinizci@hotmail.com>
2024-10-07 04:55:07 +0300

    ikinci ve üçüncü soruda girildi.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,11 +2,29 @@ let totalQuestions = 10;
 let dogruSayisi=0;
 let yanlisSayisi=0;
 
-let answer1 = prompt('CSS açılımı nedir? \n a) Computer Style Sheets \n b) Cascading Style Sheets \n c) Creative Style Sheets d) Colorful Style Sheets');
+let answer1 = prompt('CSS açılımı nedir? \n a) Computer Style Sheets \n b) Cascading Style Sheets \n c) Creative Style Sheets \n d) Colorful Style Sheets');
 if (answer1 == 'b') {
   dogruSayisi++;
   alert("✔");
 } else if (answer1 !== 'b') {
+  alert("✖");
+  yanlisSayisi++;
+}
+
+let answer2 = prompt('CSS ile bir öğeye iç boşluk eklemek için hangi kural kullanılır? \n a) margin \n b) padding \n c) border \n  d) spacing');
+if (answer2 ==  'b') {
+  dogruSayisi++;
+  alert("✔");
+}else if (answer1 !== 'b') {
+  alert("✖");
+  yanlisSayisi++;
+}
+
+let answer3 = prompt('Hangi CSS seçici tüm <p> etiketlerini seçer? \n a) .p \n b) #p \n c) p \n d) *p ');
+if (answer3 == 'c') {
+  dogruSayisi++;
+  alert("✔");
+}else if (answer1 !== 'c') {
   alert("✖");
   yanlisSayisi++;
 }
