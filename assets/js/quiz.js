602aba7162085ff58b0d9b8bc95476d5f20a6df1
hilalbaran <hilalinizci@hotmail.com>
2024-10-07 05:02:40 +0300

    dördüncü beşinci altıncı da eklendi yazım yanlışı giderildi.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -15,7 +15,7 @@ let answer2 = prompt('CSS ile bir öğeye iç boşluk eklemek için hangi kural 
 if (answer2 ==  'b') {
   dogruSayisi++;
   alert("✔");
-}else if (answer1 !== 'b') {
+}else if (answer2 !== 'b') {
   alert("✖");
   yanlisSayisi++;
 }
@@ -24,7 +24,34 @@ let answer3 = prompt('Hangi CSS seçici tüm <p> etiketlerini seçer? \n a) .p \
 if (answer3 == 'c') {
   dogruSayisi++;
   alert("✔");
-}else if (answer1 !== 'c') {
+}else if (answer3 !== 'c') {
+  alert("✖");
+  yanlisSayisi++;
+}
+
+let answer4 = prompt("CSS'de, bir öğenin görünürlüğünü gizlemek için hangi özellik kullanılır? \n a) display: none; \n b) visibility: hidden; \n c) both a and b \n d) hide: true;");
+if (answer4 == 'c') {
+  dogruSayisi++;
+  alert("✔");
+}else if (answer4 !== 'c') {
+  alert("✖");
+  yanlisSayisi++;
+}
+
+let answer5 = prompt("Flexbox içinde bir öğenin ana eksende nasıl yerleştirileceğini belirlemek için hangi özellik kullanılır? \n a) justify-content \n b) align-items  \n c) flex-direction \n d) flex-wrap");
+if (answer5 == 'a') {
+  dogruSayisi++;
+  alert("✔");
+}else if (answer5 !== 'a') {
+  alert("✖");
+  yanlisSayisi++;
+}
+
+let answer6 = prompt("Flexbox'da bir öğenin dikey olarak ortalanması için hangi özellik kullanılır? \n a) justify-content \n b) align-items  \n c) flex-direction \n d) flex-wrap");
+if (answer6 = "b") {
+  dogruSayisi++;
+  alert("✔");
+}else if (answer6 !== 'b') {
   alert("✖");
   yanlisSayisi++;
 }
